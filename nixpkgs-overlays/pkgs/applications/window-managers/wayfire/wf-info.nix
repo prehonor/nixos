200f00b5737cbc7f823c7a71972bafc22689db75
prehonor <ryx19891207@gmail.com>
2022-11-28 14:35:49 +0800

    wf-info 插件安装

--- nixpkgs-overlays/pkgs/applications/window-managers/wayfire/wf-info.nix
+++ nixpkgs-overlays/pkgs/applications/window-managers/wayfire/wf-info.nix
@@ -1,4 +1,5 @@
-{ stdenv, lib, fetchFromGitHub, meson, ninja, pkg-config, cmake, wayland, wayland-protocols, libxkbcommon, pango, cairo, wayfire, wf-config, wlroots_0_16, gtkmm3, gobject-introspection ,libpulseaudio ,alsa-lib
+{ stdenv, lib, fetchFromGitHub, meson, ninja, pkg-config, cmake, wayland, wayland-protocols, libxkbcommon, pango, cairo, wayfire, wf-config, wlroots_0_16
+, gtkmm3, gobject-introspection ,libpulseaudio , alsa-lib, udev ,xcbutilwm ,libinput
 }:
 
 stdenv.mkDerivation rec {
@@ -9,15 +10,16 @@ stdenv.mkDerivation rec {
     owner = "soreau";
     repo = "${pname}";
     rev = "022800f180b1e6adfd4824c770ea9fd94ea4b5bf";
-    sha256 = "sha256-eCga6ZdxqJYKc9yAI77fZUXOSaee8ijCE0XiJRJtDAg="; # 0000000000000000000000000000000000000000000000000000
+    sha256 = "sha256-JOqKS4PXLGw1QHtQHALznxxHTMCAN7b1Fq3MBM0GSw8="; # 0000000000000000000000000000000000000000000000000000
   };
 
   nativeBuildInputs = [ meson ninja pkg-config cmake wayland ];
   buildInputs = [
     pango cairo wayfire wf-config wlroots_0_16 wayland wayland-protocols 
-    libxkbcommon gtkmm3 gobject-introspection libpulseaudio alsa-lib
+    libxkbcommon gtkmm3 gobject-introspection libpulseaudio alsa-lib 
+    udev xcbutilwm libinput
   ];
-  
+  # dontUseCmakeConfigure = true;
   PKG_CONFIG_WAYFIRE_LIBDIR = "lib";
   PKG_CONFIG_WAYFIRE_METADATADIR = "share/wayfire/metadata";
 
