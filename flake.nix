fd3b72b29b6e29c78ad6ef0c15bd974d05db8e3d
prehonor <ryx19891207@gmail.com>
2023-01-14 12:43:31 +0800

    一个较稳定的版本

--- flake.nix
+++ flake.nix
@@ -1,22 +1,19 @@
-let 
-  nixpkgs-unstable-url = "github:NixOS/nixpkgs/nixos-unstable";
-  nixpkgs-url = "github:NixOS/nixpkgs/nixos-21.11"; 
-in
 {
-  description = "Prehonor NixOS";
-  inputs = {
-    nixpkgs.url = nixpkgs-url;
-
-    # nixos-hardware.url = github:NixOS/nixos-hardware/master;
-    # emacs-overlay.url = "github:nix-community/emacs-overlay/7ba0cd76c4e5cbd4a8ac7387c8ad2493caa800f0";
-    # nur.url = "github:nix-community/NUR";
-
-  };
-  outputs = { self, nixpkgs }@attrs: {
-    nixosConfigurations.prehonor = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs; # 将 inputs 函数的参数添加configuration.nix
-      modules = [ ./configuration.nix ];
+    description = "Prehonor NixOS";
+    inputs = {
+    
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+      # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+      # nixos-hardware.url = github:NixOS/nixos-hardware/master;
+      # emacs-overlay.url = "github:nix-community/emacs-overlay/7ba0cd76c4e5cbd4a8ac7387c8ad2493caa800f0";
+      # nur.url = "github:nix-community/NUR";
+  
     };
-  };
+    outputs = { self, nixpkgs }@attrs: {
+      nixosConfigurations.prehonor = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = attrs; # 将 inputs 函数的参数添加configuration.nix
+        modules = [ ./configuration.nix ];
+      };
+    };
 }
